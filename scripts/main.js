881b9ecc573b585ecca6b2219a8a2c87bcc4de09
CaoYu <caoyu_92@126.com>
2017-06-07 19:12:28 +0800

    该版本与chrome插件的第一版适配（必须结合使用）

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,10 @@
 
-require(["scripts/chromeSnapShot","scripts/h2c"],function(chromeSnapShot,h2c){
+require(["scripts/tmp"],function(tmp){
 
 
     var flag = false,
         omag = document.getElementsByClassName("tu")[0],
-        cvsTmp = document.createElement("canvas"),
-        ctxTmp = cvsTmp.getContext("2d"),
-        cvsShow = document.createElement("canvas"),
-        ctxShow = cvsShow.getContext("2d");
-
-
-    cvsTmp.width = 100;
-    cvsTmp.height = 80;
-
-    cvsShow.width = 200;
-    cvsShow.height = 160;
-    cvsShow.style.left = - 50 + "px";
-    cvsShow.style.top = - 40 + "px";
-    cvsShow.id = "cvsShow";
-    ctxShow.scale(2,2);
-    omag.appendChild(cvsShow);
+        theScale = 1.5;
 
 
     document.addEventListener("mousedown",function(event){
@@ -27,7 +12,6 @@ require(["scripts/chromeSnapShot","scripts/h2c"],function(chromeSnapShot,h2c){
       flag = true;
     });
     document.addEventListener("mouseup",function(event){
-      ctxTmp.clearRect(0,0,100,80);
       omag.style.display = "none";
       flag = false;
     });
@@ -43,17 +27,16 @@ require(["scripts/chromeSnapShot","scripts/h2c"],function(chromeSnapShot,h2c){
         omag.style.top = event.clientY - 90 + "px";
 
 // 计算内部canvas偏移位置：
-        var omagCvs = document.getElementById("theCvs"),
-            omagCtx = omagCvs.getContext("2d"),
-            outter = document.getElementById("outter");
+        var outter = document.getElementById("outter"),
+            snapShotImg = document.getElementById("snapShotImg");
         var posx,
             posy;
         posx = event.clientX - outter.offsetLeft;
         posy = event.clientY - outter.offsetTop;
 
-        var tmpImg = omagCtx.getImageData(posx-50,posy-40,100,80);
-        ctxTmp.putImageData(tmpImg,0,0);
-        ctxShow.drawImage(cvsTmp,0,0);
+        snapShotImg.style.left = - posx + 50 + 'px';
+        snapShotImg.style.top = - posy + 40 + 'px';
+
 
       }
 
