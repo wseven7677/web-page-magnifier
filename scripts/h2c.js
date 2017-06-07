8c90c4b4b2330bc0604ed7095cf569b397cbb0f7
CaoYu <caoyu_92@126.com>
2017-06-07 10:26:04 +0800

    canvas转移放大，放大镜功能已实现。后续：考虑性能，打算放弃h2c转用chrome截图api

--- scripts/h2c.js
+++ scripts/h2c.js
@@ -4,25 +4,19 @@ define(function(){
   var otu = document.getElementsByClassName("tu")[0],
       cvs = document.createElement("canvas"),
       outter = document.getElementById("outter");
-  otu.appendChild(cvs);
+
+  outter.parentNode.appendChild(cvs);
   cvs.id = "theCvs";
+  cvs.style.display = "none";
 
-  cvs.width = outter.offsetWidth * 2;
-  cvs.height = outter.offsetHeight * 2;
-  cvs.style.width = outter.offsetWidth;
-  cvs.style.height = outter.offsetHeight;
-
-  // var context = cvs.getContext("2d");//2d上下文
-
+  var context = cvs.getContext("2d");//2d上下文
 
   html2canvas(outter,{
     canvas: cvs,
-    // height: 1000,
-    // width: 1000,
     onrendered: function(c){
       // cvs.width *= 2;
       // cvs.height *= 2;
-    //   c.getContext("2d").context.scale(2,2);
+      // c.getContext("2d").context.scale(2,2);
     }
   });
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,34 @@
 
-require(["scripts/h2c"],function(h2c){
+require(["scripts/chromeSnapShot","scripts/h2c"],function(chromeSnapShot,h2c){
 
-    var flag = false;
+
+    var flag = false,
+        omag = document.getElementsByClassName("tu")[0],
+        cvsTmp = document.createElement("canvas"),
+        ctxTmp = cvsTmp.getContext("2d"),
+        cvsShow = document.createElement("canvas"),
+        ctxShow = cvsShow.getContext("2d");
+
+
+    cvsTmp.width = 100;
+    cvsTmp.height = 80;
+
+    cvsShow.width = 200;
+    cvsShow.height = 160;
+    cvsShow.style.left = - 50 + "px";
+    cvsShow.style.top = - 40 + "px";
+    cvsShow.id = "cvsShow";
+    ctxShow.scale(2,2);
+    omag.appendChild(cvsShow);
+
 
     document.addEventListener("mousedown",function(event){
-
+      omag.style.display = "block";
       flag = true;
     });
     document.addEventListener("mouseup",function(event){
-
+      ctxTmp.clearRect(0,0,100,80);
+      omag.style.display = "none";
       flag = false;
     });
 
@@ -19,26 +39,21 @@ require(["scripts/h2c"],function(h2c){
         event.stopPropagation();
 
 // 计算外框位置：
-        var omag = document.getElementsByClassName("tu")[0];
         omag.style.left = event.clientX - 50 + "px";
         omag.style.top = event.clientY - 90 + "px";
 
 // 计算内部canvas偏移位置：
         var omagCvs = document.getElementById("theCvs"),
+            omagCtx = omagCvs.getContext("2d"),
             outter = document.getElementById("outter");
         var posx,
             posy;
         posx = event.clientX - outter.offsetLeft;
         posy = event.clientY - outter.offsetTop;
 
-        omagCvs.style.left = - posx + 50 + "px";
-        omagCvs.style.top = - posy + 40 + "px";
-
-
-        // var ocanvas = document.getElementById("theCvs");
-        // var context = ocanvas.getContext("2d");//2d上下文
-        //
-        // ocanvas.scale(2,2);
+        var tmpImg = omagCtx.getImageData(posx-50,posy-40,100,80);
+        ctxTmp.putImageData(tmpImg,0,0);
+        ctxShow.drawImage(cvsTmp,0,0);
 
       }
 
